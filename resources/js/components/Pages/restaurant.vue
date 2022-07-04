90fd5597d89a3f978d409d84522990f31aae68cd
Andrea <onemanshow2099@gmail.com>
2022-07-04 16:22:43 +0200

    push api 2

--- resources/js/components/Pages/restaurant.vue
+++ resources/js/components/Pages/restaurant.vue
@@ -1,3 +1,33 @@
-<style lang="postcss" scoped>
+<template>
+    <div>
+        <div v-for="user in users" :key="user.id">
+            <p>{{user.name}}</p>
+                </div>
+                
+    </div>
+</template>
 
-</style>
+<script>
+import axios from 'axios';
+export default{
+     name:"UsersList",
+     
+     data(){
+        return{
+            users: [],
+            
+        }
+     },
+     methods: {
+        getUsers(){
+            axios.get('http://127.0.0.1:8000/api/restaurants')
+            .then((res)=> {
+               this.restaurants = res.data.restaurants;
+            })
+        }
+     },
+     mounted(){
+        this.getUser();
+     }
+}
+</script>
