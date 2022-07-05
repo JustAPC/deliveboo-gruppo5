376cfe2ebd8f717cf9341a9ef4b760309b9396cd
StefanoMRC <stefano.mrc92@gmail.com>
2022-07-05 11:06:21 +0200

    sistemati seeder e migration user_type

--- database/migrations/2022_07_05_084647_user_type_table.php
+++ database/migrations/2022_07_05_084647_user_type_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRestaurantTypeTable extends Migration
+class UserTypeTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateRestaurantTypeTable extends Migration
      */
     public function up()
     {
-        Schema::create('restaurant_type', function (Blueprint $table) {
+        Schema::create('user_type', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
@@ -30,6 +30,6 @@ class CreateRestaurantTypeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('restaurant_type');
+        Schema::dropIfExists('user_type');
     }
 }
--- database/seeds/UserTypeSeeder.php
+++ database/seeds/UserTypeSeeder.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Type;
+use App\User;
 
-class RestaurantTypeTableSeeder extends Seeder
+class UserTypeSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -11,73 +13,73 @@ class RestaurantTypeTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 1,
                 'type_id' => 5,
             ],
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 1,
                 'type_id' => 6,
             ],
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 2,
                 'type_id' => 5,
             ],
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 2,
                 'type_id' => 6,
             ],
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 3,
                 'type_id' => 3,
             ]
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 4,
                 'type_id' => 10,
             ]
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 5,
                 'type_id' => 3,
             ]
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 6,
                 'type_id' => 3,
             ]
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 7,
                 'type_id' => 1,
             ]
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 8,
                 'type_id' => 1,
             ]
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 9,
                 'type_id' => 3,
             ]
         );
-        DB::table('restaurant_type')->insert(
+        DB::table('user_type')->insert(
             [
                 'user_id' => 10,
                 'type_id' => 9,
