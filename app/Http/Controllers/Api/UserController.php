a706acff4fde5ee71e18e6cdf5dc50563f5faefd
StefanoMRC <stefano.mrc92@gmail.com>
2022-07-05 11:56:31 +0200

    fix API

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
+use App\Models\Type;
 
 class UserController extends Controller
 {
@@ -15,7 +16,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $restaurants = User::with('Dishes','Type')->get();
+        $restaurants = User::with('Dishes', 'UserType')->get();
 
         return response()->json(compact('restaurants'));
     }
--- database/migrations/2022_07_05_095018_crate_type_user_table.php
+++ database/migrations/2022_07_05_095018_crate_type_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UserTypeTable extends Migration
+class CrateTypeUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class UserTypeTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_type', function (Blueprint $table) {
+        Schema::create('type_user', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->unsignedBigInteger('type_id');
@@ -30,6 +30,6 @@ class UserTypeTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('user_type');
+        Schema::dropIfExists('type_user');
     }
 }
