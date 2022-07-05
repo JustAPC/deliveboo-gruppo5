d24ee7ec0c190de7be914bb23f601c528495bdbf
StefanoMRC <stefano.mrc92@gmail.com>
2022-07-05 10:03:10 +0200

    sistemazione usercontrolle front

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $restaurants = User::all();
+        $restaurants = User::with('Dishes')->get();
 
         return response()->json(compact('restaurants'));
     }
