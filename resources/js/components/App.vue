b7845df0fec3038d825cc5612357c59ca3486b73
StefanoMRC <stefano.mrc92@gmail.com>
2022-07-05 10:11:28 +0200

    ok

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -17,7 +17,7 @@
 <script>
 
 import Home from "./Pages/Home.vue";
-import Restaurants from "./Pages/Restaurant.vue";
+import Restaurant from "./Pages/Restaurant.vue";
 import Header from "./Pages/Header.vue";
     export default{
         name:'App',
