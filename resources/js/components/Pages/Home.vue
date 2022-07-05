d27f1eb67a3ba49ef05f122e025c01956304489e
StefanoMRC <stefano.mrc92@gmail.com>
2022-07-05 15:03:24 +0200

    modifiche header e home

--- resources/js/components/Pages/Home.vue
+++ resources/js/components/Pages/Home.vue
@@ -1,15 +1,49 @@
 <template>
-    <div>
-        <h1>prova</h1>
+    <div class="container">
+
+        <div class="row justify-content-end">   
+             <select class="mt-3" >
+                    <option v-for="type in types" :key="type.id">{{type.name}}</option>
+                </select>
+
+        </div>
+        
+       
+
+
+
     </div>
+   
+
+
+    
 </template>
 
 <script>
+    import axios from 'axios';
+    export default {
+        name: 'Home',
+        components: {
 
-export default {
-    name:'Home',
-    components:{
-         
+        },
+        data() {
+            return {
+                types: [],
+
+            }
+        },
+        methods: {
+            getType() {
+                axios.get('http://127.0.0.1:8000/api/types')
+                    .then((res) => {
+                        this.types = res.data.types;
+                    })
+            }
+        },
+        mounted() {
+            this.getType();
         }
-}
+
+    }
+
 </script>
--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::namespace('Api')->group(function(){
 Route::get('/restaurants', 'UserController@index');
+Route::get('/types', 'TypeController@index');
 });
