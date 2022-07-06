dd456e90bc761ee39586d3e167fa08aa62322cef
catclou <caterina.moccia@gmail.com>
2022-07-06 16:19:18 +0200

    🎨create dishes + validazione con suggerimenti

--- resources/views/admin/dishes/create.blade.php
+++ resources/views/admin/dishes/create.blade.php
@@ -6,6 +6,7 @@
 
 
 @section('content')
+
     @if ($errors->any())
         {{-- Se sono presenti errori backend --}}
         <div class="alert alert-danger">
@@ -17,41 +18,129 @@
         </div>
     @endif
 
+    
 
-    <div class="container">
-        <form action="{{ route('admin.dishes.store') }}" method="POST" enctype="multipart/form-data">
+    <div class="container mt-5 pt-5">
+
+        <h1 class="mb-5 pt-5">Inserisci un nuovo piatto</h1>
+
+        <form action="{{ route('admin.dishes.store') }}" method="POST" enctype="multipart/form-data" class="needs-validation" novalidate>
             @csrf
-            <label for="name">Nome del piatto:</label>
-            <input type="text" id="name" name="name" value="{{ old('name') }}" class="mx-3">
+            
+            {{-- Riga nome --}}
+            <div class="form-group floating">
+                <input type="text" id="name" name="name" required value="{{ old('name') }}" class="form-control floating">
+                <div class="invalid-feedback">
+                    Inserisci il nome del piatto
+                </div>
+                <label for="name">Nome del piatto</label>
+            </div>
+            
+            {{-- riga quantità + ingredienti + disponibile + prezzo --}}
+            <div class="form-row">
+                <div class="form-group floating col-md-1">
+                    <input type="number" id="quantity" name="quantity" min="1" value="1" 
+                    value="{{ old('quantity') }}" class="form-control floating">
+                    <label for="quantity">Quantità:</label>
+                </div>
+    
+                <div class="form-group floating col-md-8">
+                    <input type="text" id="ingredients" name="ingredients" required value="{{ old('ingredients') }}" class="form-control floating">
+                    <div class="invalid-feedback">
+                        Inserisci gli ingredienti
+                    </div>
+                    <label for="ingredients">Ingredienti</label>
+                </div>
 
-            <label for="quantity">Quantità:</label>
+                <div class="form-group floating col-md-1">
+                    <select name="available" id="available" class="form-control floating">
+                        <option value="1">Si</option>
+                        <option value="0">No</option>
+                    </select>
+                    <label for="available">Disponibile:</label>
+                </div>
+
+                <div class="form-group floating col-md-2">
+                    <input type="number" id="price" name="price" required value="{{ old('price') }}" class="form-control floating">
+                    <div class="invalid-feedback">
+                        Inserisci il prezzo
+                    </div>
+                    <label for="name">Prezzo in €</label>
+                </div>
+            </div>
+
+            {{-- riga descrizione + categoria piatto --}}
+            <div class="form-row">
+                <div class="form-group floating col-md-8">
+                    <input type="text" id="description" name="description" value="{{ old('description') }}" class="form-control floating">
+                    <label for="description">Descrizione:</label>
+                </div>
+
+                <div class="form-group floating col-md-4">
+                    <select required name="dishcategory_id" id="dishcategory_id" class="form-control floating">
+                        <option required disabled selected>Scegli una categoria...</option>
+                        @foreach ($dishcategories as $category)
+                            <option required value="{{ $category->id }}">{{ $category->name }}</option>
+                        @endforeach
+                    </select>
+                    <label for="dishcategory_id">Categoria piatto</label>
+                </div>
+            </div>
+
+            {{-- riga immagine caricamento file + url --}}
+            <div class="form-row d-flex align-items-center">
+                <div class="form-group col-md-4">
+                    <input id="myfiles" name="image" type="file" accept="image/*">
+                    <button id="file_remove" class="btn btn-outline-deliveboo" disabled>Rimuovi</button>
+                    <label for="image">Immagine del piatto</label>
+                </div>
+                <div class="form-group col-md-8">
+                    <input type="url" name="image" id="image_url" class="form-control">
+                    <label for="image_url">oppure inserisci url</label>
+                </div>
+            </div>
+
+            {{-- submit button --}}
+            <div class="form-group mt-5 d-flex justify-content-center">
+                <button type="submit" class="btn btn-deliveboo w-25 h-75">Invia</button>
+            </div>
+
+
+
+            {{-- VECCHIO INPUT QUANTITà --}}
+            {{-- <label for="quantity">Quantità:</label>
             <input type="number" id="quantity" name="quantity" min="1" value="1"
-                value="{{ old('quantity') }}" class="mx-3">
+                value="{{ old('quantity') }}" class="mx-3"> --}}
 
-            <label for="ingredients">Ingredienti:</label>
-            <input type="text" id="ingredients" name="ingredients" value="{{ old('ingredients') }}" class="mx-3">
+            {{-- VECCHIO INPUT INGREDIENTI --}}
+            {{-- <label for="ingredients">Ingredienti:</label>
+            <input type="text" id="ingredients" name="ingredients" value="{{ old('ingredients') }}" class="mx-3"> --}}
 
-            <label for="available">Disponibile:</label>
+            {{-- VECCHIO INPUT DISPONIBILE --}}
+            {{-- <label for="available">Disponibile:</label>
             <select name="available" id="available">
                 <option value="1">Si</option>
                 <option value="0">No</option>
-            </select>
+            </select> --}}
 
-            <label for="name">Prezzo:</label>
-            <input type="text" id="price" name="price" value="{{ old('price') }}" class="mx-3">
+            {{-- VECCHIO INPUT PREZZO --}}
+            {{-- <label for="name">Prezzo:</label>
+            <input type="text" id="price" name="price" value="{{ old('price') }}" class="mx-3"> --}}
 
-            <label for="description">Descrizione:</label>
-            <input type="text" id="description" name="description" value="{{ old('description') }}" class="mx-3">
+            {{-- VECCHIO INPUT DESCRIZIONE --}}
+            {{-- <label for="description">Descrizione:</label>
+            <input type="text" id="description" name="description" value="{{ old('description') }}" class="mx-3"> --}}
 
-            <span>Categoria Piatto:</span>
+            {{-- VECCHIO INPUT CATEGORIA --}}
+            {{-- <span>Categoria Piatto:</span>
             <select name="dishcategory_id" id="dishcategory_id">
                 <option disabled selected>Scegli una categoria...</option>
                 @foreach ($dishcategories as $category)
                     <option value="{{ $category->id }}">{{ $category->name }}</option>
                 @endforeach
-            </select>
+            </select> --}}
 
-            <div class="mt-5" style="padding-top: 200px">
+            {{-- <div class="mt-5" style="padding-top: 200px">
                 <p>Immagine del piatto:</p>
                 <input id="myfiles" name="image" type="file" accept="image/*">
                 <button id="file_remove" disabled>Rimuovi</button>
@@ -60,9 +149,32 @@
                     <label for="image_url">oppure</label>
                     <input type="url" name="image" id="image_url" class="mx-3">
                 </div>
-            </div>
+            </div> --}}
 
-            <button type="submit" class="mt-5">Invia</button>
+            {{-- <button type="submit" class="mt-5">Invia</button> --}}
         </form>
     </div>
 @endsection
+
+
+{{-- Validazione client-side --}}
+<script>
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+          }, false);
+        });
+      }, false);
+    })();
+    </script>
