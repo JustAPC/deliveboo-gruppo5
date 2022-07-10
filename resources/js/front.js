600d8c3fbb5a6aedc4cfe229897d06f553d78e74
JustAPC <andreapontillo.dev@gmail.com>
2022-07-10 18:02:23 +0200

    aggiunto braintree

--- resources/js/front.js
+++ resources/js/front.js
@@ -1,6 +1,8 @@
 window.Vue = require("vue");
 
 import Vue from "vue";
+import vue-braintree from 'vue-braintree';
+Vue.use(vue-braintree)
 
 import router from "./routes.js";
 import App from "./components/App.vue";
