b99f4c86c8e3430015b116f0e6d9f8df67d1f094
Andrea <onemanshow2099@gmail.com>
2022-07-13 11:13:00 +0200

    finito

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -11,29 +11,114 @@
         <div class="rounded bg-white p-5">
 
             <div class="text-center align-self-center">
-                <h3 class="user">Benvenuto {{$user->name}}</h3>
+                <h3 class="user">Benvenuto</h3>
             </div>
 
             <div class="text-center">
-                <h1 class="">{{ $user->restaurant_name }}</h1>
-                <div class="d-flex justify-content-center mb-3">
+                <h1 class="font-italic">{{ $user->restaurant_name }}</h1>
+                <div class="d-flex justify-content-center mb-3 badgeTesto">
                     @foreach ($user->UsersType as $type)
-                    <span class="badge badge-primary mr-3">{{$type->name}}</span>
+                    <span class="badge badge-primary mr-3 badge-pill">{{$type->name}}</span>
                     @endforeach
                 </div>
 
                 <p class="d-flex justify-content-center">
-                    <a href="{{route('admin.dishes.index')}}" class="btn-cerca-vedi mr-5">Visualizza Piatti</a>
-                    <a href="{{route('admin.orders.index')}}" type="button" class="btn-cerca-vedi">Visualizza Ordini</a>
+                    <a href="{{route('admin.dishes.index')}}" class="btn-cerca-vedi ">Visualizza Piatti</a>
+                    
                 </p>
             </div>
+            {{-- slideshow --}}
+            
+            <div class="pannello-slide">
+             
+              <div id="posto-immagine">
+               
+              </div>
+               <span id="sinistra-freccia">&#10096;</span>
+              <span id="destra-freccia" ">&#10097;</span>
+            </div>
+            
+            {{-- fine --}}
+            <p class="d-flex justify-content-center">
+                   
+                    <a href="{{route('admin.orders.index')}}" type="button" class="btn-cerca-vedi">Visualizza Ordini</a>
+                </p>
 
+                <div class="pannelloOrdini">
+                    <span class="spanOrdini">Ordini completati e pagati: <span id="orC"></span></span>
+                    <span class="spanOrdini">Ordini da completare o da pagare: <span id="orP"></span></span>
+                </div>
         </div>
-
+        
     </div>
-
+    @php
+        
+    @endphp
 </section>
 
+
+<script>
+let frecciaDestra = document.getElementById('destra-freccia');
+let frecciaSinistra = document.getElementById('sinistra-freccia');
+let indexPiatto = 0;
+let piatti = <?php echo json_encode($dishes); ?>;
+let ordini = <?php echo json_encode($orders); ?>;
+let imagePiatto = piatti[indexPiatto]['image'];
+let nomePiatto = piatti[indexPiatto]['name'];
+document.getElementById("posto-immagine").innerHTML = ` <img src="${imagePiatto}" alt="">
+<p>${nomePiatto}</p>`;
+
+
+frecciaDestra.addEventListener("click", function(){
+    if(indexPiatto == piatti.length -1){
+        indexPiatto = 0;
+    } else {
+        indexPiatto++;
+    }
+    
+    let imagePiatto2 = piatti[indexPiatto]['image'];
+    let nomePiatto2 = piatti[indexPiatto]['name'];
+    document.getElementById("posto-immagine").innerHTML = ` <img src="${imagePiatto2}" alt="">
+    <p>${nomePiatto2}</p>`;
+    console.log(indexPiatto)
+});
+frecciaSinistra.addEventListener("click", function(){
+    if(indexPiatto == 0){
+        indexPiatto = piatti.length -1;
+    } else {
+        indexPiatto = indexPiatto -1;
+    }
+    
+    let imagePiatto2 = piatti[indexPiatto]['image'];
+    let nomePiatto2 = piatti[indexPiatto]['name'];
+    document.getElementById("posto-immagine").innerHTML = ` <img src="${imagePiatto2}" alt="">
+    <p>${nomePiatto2}</p>`;
+    console.log(indexPiatto)
+})
+console.log(ordini)
+
+    let risultatoOrdiniComple = 0;
+    let nonCompletati = 0;
+    for(i = 0; i < ordini.length; i++){
+        if((ordini[i]['completed'] == true) && (ordini[i]['payment_received'] == true)){
+            risultatoOrdiniComple++;
+        } 
+        
+        
+    }
+    for(i = 0; i < ordini.length; i++){
+        if((ordini[i]['completed'] != true) || (ordini[i]['payment_received'] != true)){
+           nonCompletati++;
+        } 
+        
+        
+    }
+
+console.log(risultatoOrdiniComple);
+console.log(nonCompletati)
+document.getElementById("orC").innerHTML = `${risultatoOrdiniComple}`
+document.getElementById("orP").innerHTML = `${nonCompletati}`
+</script>
 @endsection
 
 <style>
@@ -77,5 +162,22 @@
         padding: 6px 12px;
         margin-bottom: 0;
     }
+.badgeTesto {
+    font-size: 1.5em;
+}
 
+.pannelloOrdini {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin-top: 20px;
+}
+.spanOrdini {
+    padding: 8px;
+    background-color: #e98c22;
+    border-radius: 15px;
+    font-size: 1.1em;
+    font-weight: bold;
+}
 </style>
