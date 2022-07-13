80e40b7fea77a3b4c984fa7a33d215e4929ed3d2
Andrea <onemanshow2099@gmail.com>
2022-07-13 18:21:17 +0200

    register finito

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,16 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="w-50 m-auto">
-
-    <h1 class="my-4">Registrazione</h1>
-    <form method="POST" action="{{ route('register') }}" class="needs-validation" novalidate>
+<div class="contenitoreRegistrazione">
+  <div class="bg"></div>
+    <div class="bg bg2"></div>
+    <div class="bg bg3"></div>
+    {{-- <h1 class="">Registrazione</h1> --}}
+    <form method="POST" action="{{ route('register') }}" class="needs-validation formRegister" novalidate>
         @csrf
-
+        <h1 class="titoloRegistrazione">Registrazione</h1>
         {{-- Nome Utente --}}
-        <div class="form-group mb-4">
+        <div class="barraRegister">
             <label for="name">{{ __('Nome del ristoratore') }}</label>
-            <input id="name" type="text" class="form-control @error('name') is-invalid @enderror " name="name"
+            <input id="name" type="text" class=" @error('name') is-invalid @enderror " name="name"
                 value="{{ old('name') }}" autocomplete="name" autofocus>
             <div class="invalid-feedback">
                 Inserisci il nome utente
@@ -19,9 +21,9 @@
         </div>
 
         {{-- Nome Attività --}}
-        <div class="form-group mb-4 ">
+        <div class=" barraRegister">
             <label for="restaurant_name">{{ __('Nome Attività') }}</label>
-            <input id="restaurant_name" type="text" class="form-control @error('restaurant_name') is-invalid @enderror "
+            <input id="restaurant_name" type="text" class=" @error('restaurant_name') is-invalid @enderror "
                 name="restaurant_name" value="{{ old('restaurant_name') }}" required autocomplete="restaurant_name"
                 autofocus>
             <div class="invalid-feedback">
@@ -32,9 +34,9 @@
 
 
         {{-- Indirizzo --}}
-        <div class="form-group mb-4">
+        <div class="barraRegister">
             <label for="address">{{ __('Indirizzo') }}</label>
-            <input id="address" type="text" class="form-control @error('address') is-invalid @enderror " name="address"
+            <input id="address" type="text" class=" @error('address') is-invalid @enderror " name="address"
                 value="{{ old('address') }}" required autocomplete="address" autofocus>
             <div class="invalid-feedback">
                 Inserisci un indirizzo
@@ -42,11 +44,11 @@
         </div>
 
         {{--Stato città - Zip --}}
-        <div class="form-row mb-4">
+        
             {{-- Stato --}}
-            <div class="form-group col-6 col-md-4">
+            <div class="barraRegister">
                 <label for="state">{{ __('Stato') }}</label>
-                <input id="state" type="text" class="form-control @error('state') is-invalid @enderror " name="state"
+                <input id="state" type="text" class=" @error('state') is-invalid @enderror " name="state"
                     value="{{ old('state') }}" required autocomplete="state" autofocus>
                 <div class="invalid-feedback">
                     Inserisci uno stato
@@ -54,9 +56,9 @@
             </div>
 
             {{-- Città --}}
-            <div class="form-group col-6 col-md-4">
+            <div class="barraRegister">
                 <label for="city">{{ __('Città') }}</label>
-                <input id="city" type="text" class="form-control @error('city') is-invalid @enderror " name="city"
+                <input id="city" type="text" class=" @error('city') is-invalid @enderror " name="city"
                     value="{{ old('city') }}" required autocomplete="city" autofocus>
                 <div class="invalid-feedback">
                     Inserisci una città
@@ -64,20 +66,20 @@
             </div>
 
             {{-- Codice Postale --}}
-            <div class="form-group col-6 offset-md-1 col-md-3">
+            <div class="barraRegister">
                 <label for="zip">{{ __('Codice postale') }}</label>
-                <input id="zip" type="text" class="form-control @error('zip') is-invalid @enderror " name="zip"
+                <input id="zip" type="text" class=" @error('zip') is-invalid @enderror " name="zip"
                     value="{{ old('zip') }}" required autocomplete="zip" autofocus>
                 <div class="invalid-feedback">
                     Inserisci un codice postale valido
                 </div>
             </div>
-        </div>
+        
 
         {{-- Numero di telefono --}}
-        <div class="form-group mb-4 ">
+        <div class=" barraRegister">
             <label for="phone_number">{{ __('Telefono') }}</label>
-            <input id="phone_number" type="text" class="form-control @error('phone_number') is-invalid @enderror "
+            <input id="phone_number" type="text" class=" @error('phone_number') is-invalid @enderror "
                 name="phone_number" value="{{ old('phone_number') }}" required autocomplete="phone_number" autofocus>
             <div class="invalid-feedback">
                 Inserisci un numero di telefono valido
@@ -86,9 +88,9 @@
         </div>
 
         {{-- P IVA --}}
-        <div class="form-group mb-4">
+        <div class="barraRegister">
             <label for="vat">{{ __('P.IVA') }}</label>
-            <input id="vat" type="text" class="form-control @error('vat') is-invalid @enderror " name="vat"
+            <input id="vat" type="text" class=" @error('vat') is-invalid @enderror " name="vat"
                 value="{{ old('vat') }}" required autocomplete="vat" autofocus>
             <div class="invalid-feedback">
                 Inserisci un numero di partita iva valido
@@ -97,7 +99,7 @@
         </div>
 
         {{-- Tipo di ristorante --}}
-        <div class="mb-4">
+        <div class="barraRegister">
             <p>Categorie</p>
             <div class="border p-2">
                 @foreach ($types as $type)
@@ -117,16 +119,16 @@
         </div>
 
         {{-- Immagine ristorante --}}
-        <div class="form-group mb-4">
+        <div class="barraRegister">
             <label for="restaurant_img">{{ __('Url immagine ristorante') }}</label>
-            <input id="restaurant_img" type="text" class="form-control @error('restaurant_img') is-invalid @enderror "
+            <input id="restaurant_img" type="text" class=" @error('restaurant_img') is-invalid @enderror "
                 name="restaurant_img" value="{{ old('restaurant_img') }}" autocomplete="restaurant_img" autofocus>
         </div>
 
         {{-- Email --}}
-        <div class="form-group mb-4">
+        <div class="barraRegister">
             <label for="email">{{ __('E-Mail') }}</label>
-            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror " name="email"
+            <input id="email" type="email" class=" @error('email') is-invalid @enderror " name="email"
                 value="{{ old('email') }}" required autocomplete="email">
             <div class="invalid-feedback">
                 Inserisci una mail valida
@@ -134,27 +136,27 @@
         </div>
 
         {{-- Password --}}
-        <div class="form-group mb-4">
+        <div class="barraRegister">
             <label for="password">{{ __('Password') }}</label>
-            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror "
+            <input id="password" type="password" class=" @error('password') is-invalid @enderror "
                 name="password" required autocomplete="new-password">
         </div>
 
         {{-- Password confirm --}}
-        <div class="form-group mb-4">
+        <div class="barraRegister">
             <label for="password-confirm">{{ __('Confirm Password') }}</label>
-            <input id="password-confirm" type="password" class="form-control " name="password_confirmation" required
+            <input id="password-confirm" type="password" class="" name="password_confirmation" required
                 autocomplete="new-password">
         </div>
 
         {{-- Button registe --}}
-        <div class="form-group row mb-4">
-            <div class="col-6">
-                <button type="submit" class="btn btn-primary">
+        
+            <div class="">
+                <button type="submit" class="submitRegister">
                     {{ __('Register') }}
                 </button>
             </div>
-        </div>
+        
     </form>
 
 </div>
