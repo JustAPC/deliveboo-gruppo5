b2ae404cca80143de5c104847788754ca053f549
JustAPC <andreapontillo.dev@gmail.com>
2022-07-14 13:36:37 +0200

    merge completato

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -6,12 +6,26 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use App\Models\Dish;
+use App\Models\Dishcategory;
+use App\Models\Order;
 
 class HomeController extends Controller
 {
     public function index(User $user)
     {
         $user = Auth::user();
-        return view('admin.home', compact('user'));
+
+
+        $currentUserId = Auth::user()->id;
+
+
+        $dishes = Dish::where('user_id', '=', $currentUserId)->get();
+
+        $orders = Order::orderBy('updated_at', 'DESC')->where('user_id', '=', $currentUserId)->get();
+
+
+
+        return view('admin.home', compact('user', 'dishes', 'orders'));
     }
 }
