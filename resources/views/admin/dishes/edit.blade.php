f6141995faaf38d0e59cab88606de71ce85bcb31
JustAPC <andreapontillo.dev@gmail.com>
2022-07-15 18:34:20 +0200

    modifiche pulsanti

--- resources/views/admin/dishes/edit.blade.php
+++ resources/views/admin/dishes/edit.blade.php
@@ -102,12 +102,23 @@
 
             {{-- riga immagine caricamento file + url --}}
             <div class="form-row d-flex align-items-center">
-                <div class="form-group col-md-4">
-                    <input id="myfiles" name="image" type="file" accept="image/*">
-                    <button id="file_remove" class="btn btn-orange mt-3" disabled>Rimuovi</button>
-                    <div id="img-preview" class="pt-5"></div>
+                <div class="form-group col-md-4 position-relative">
+                    <div class="d-flex align-items-center">
+                        <input id="myfiles" name="image" type="file" class="inputfile" accept="image/*">
+                        <label for="myfiles">
+                            <figure class="text-center"><svg xmlns="http://www.w3.org/2000/svg" width="20"
+                                    height="17" viewBox="0 0 20 17">
+                                    <path
+                                        d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z">
+                                    </path>
+                                </svg></figure><span>Choose a file</span>
+                        </label>
+                        <button id="file_remove" class="btn btn-orange ml-5" disabled>Rimuovi</button>
+                        <div id="img-preview" class="pt-5"></div>
+                    </div>
                 </div>
-                <div class="form-group col-md-8">
+
+                <div class="form-group col-md-8 mt-5">
                     <input type="url" name="image" id="image_url" class="form-control" value="{{ old('image') }}">
                     <label for="image_url">oppure inserisci url</label>
                 </div>
