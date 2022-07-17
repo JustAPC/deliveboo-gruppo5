57c24a3c711a020183e2b2e66852bb90c8b152a4
catclou <caterina.moccia@gmail.com>
2022-07-17 12:19:06 +0200

    🐛risolto bug minore

--- resources/js/components/Pages/Payment.vue
+++ resources/js/components/Pages/Payment.vue
@@ -7,7 +7,8 @@
         {{ dropIn() }}
       </div>
 
-      <h2 class="text-center mt-5 text-deliveboo">Stai pagando € {{ total_price }}</h2>
+      <h2 class="text-center mt-5 text-deliveboo">Stai pagando € {{total_price_payment}}</h2>
+
       <div class="d-flex justify-content-center">
         <button
           class="btn btn-invia rounded mt-3 py-1 px-3 text-white font-bold"
@@ -35,7 +36,7 @@
         restaurant_name: this.$route.params.restaurant_name,
         restaurant_email: this.$route.params.restaurant_email,
         piatti: this.$route.params.piatti,
-        total_price: this.$route.params.ordine.total_price,
+        total_price_payment: this.$route.params.ordine.total_price
       };
     },
     components: { Loader },
